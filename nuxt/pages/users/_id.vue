584a65882040662c4ef6efa9229744ea7c798924
arie0703 <[ari0703sophia@gmail.com]>
2021-10-02 16:34:42 +0900

    マイチャレンジのカードのUIを別コンポーネントに分ける

--- nuxt/pages/users/_id.vue
+++ nuxt/pages/users/_id.vue
@@ -18,71 +18,9 @@
             width="275"
             class="stamp-card"
             >
-                <v-container>
-                    <v-card-text>
-                        {{ challenge.title }}
-                        <v-btn
-                        icon
-                        class="float-right delete-btn"
-                        @click="showAlert = true; deleteId=challenge.id"
-                        >
-                            <v-icon>mdi-minus</v-icon>
-                        </v-btn>
-
-                    </v-card-text>
-
-                    <v-card-text>
-                        達成日数: {{ challenge.cleared}} 
-                        継続: {{ challenge.continuation }}
-
-                    </v-card-text>
-
-                    <Stamps :cleared="challenge.cleared" :goal="challenge.goal"></Stamps>
-
-                    <div class="private-area" v-if="$auth.user.id == $route.params.id">
-
-
-                        <div v-if="challenge.is_started || challenge.done_at">
-                            <v-card-text>{{ challenge.done_at }}</v-card-text>
-                            <v-btn 
-                            @click="pushDone(challenge.id, challenge.cleared, challenge.continuation, challenge.done_at, challenge.goal)"
-                            v-if="!isDoneToday(challenge.done_at)"
-                            >
-                                Done
-                            </v-btn>
-
-                            <p v-if="isDoneToday(challenge.done_at)">今日は達成済み</p>
-                            <p>{{challenge.is_done}}</p>
-                        </div>
-
-                        <div v-if="!challenge.is_started && !challenge.done_at">
-                            <v-card-text>ボタンを押してスタート！</v-card-text>
-                            <v-btn @click="start(challenge.id)">START</v-btn>
-                        </div>
-                    </div>
-                </v-container>
+                <CardView :challenge="challenge" :yesterday="yesterday" :today="today" @updateData="getMyChallenges"></CardView>
             </v-card>
         </v-row>
-
-
-        <v-dialog
-        v-model="showAlert"
-        width="500"
-        >
-            
-            <v-card-text v-if="showAlert">
-                <v-alert
-                    color="orange"
-                    type="info"
-                >
-                削除したら戻せないよ！
-                <p>id: {{deleteId}}</p>
-                </v-alert>
-                <v-btn @click="destroy()">削除</v-btn>
-                <v-btn v-on:click="showAlert=false">キャンセル</v-btn>
-            </v-card-text>
-            
-        </v-dialog>
     </v-container>
 </template>
 
@@ -113,13 +51,13 @@
 }
 </style>
 <script>
-import Stamps from '../challenges/stamps.vue'
 import UserEdit from './edit.vue'
+import CardView from '../challenges/cardview.vue'
 export default {
     auth: false,
     components: {
-        Stamps,
         UserEdit,
+        CardView,
     },
     data: () => {
         return {
@@ -130,7 +68,6 @@ export default {
             cleared: 0,
             yesterday: new Date(),
             today: new Date(),
-            showAlert: false,
             deleteId: 0,
         }
     },
@@ -224,73 +161,7 @@ export default {
                 console.log("error.")
             })
         },
-        start(id) {
-            const url = `/api/v1/challenges/${id}`
 
-            //paramsの値をしっかり初期値にしておく
-            this.params.challenge.done_at = null 
-            this.params.challenge.cleared = 0
-            this.params.challenge.continuation = 0
-            this.params.challenge.is_done = false
-
-            this.$axios.put(url, this.params)
-                .then((res) => {
-                    this.getMyChallenges() // challengesを再取得  
-                })
-                .catch((err) => {
-                    console.log("error.")
-                })  
-        },
-        destroy() {
-            const url = `/api/v1/challenges/${this.deleteId}`
-
-            this.$axios.delete(url)
-                .then(() => {
-                    this.getMyChallenges()
-                    this.showAlert = false;
-                
-                })
-                .catch((err) => {
-                const message = err.response.data
-                this.$bvToast.toast(message, {
-                    title: 'エラー',
-                    variant: 'danger'
-                })
-            })
-        },
-        pushDone(id, cleared, continuation, done_at, goal) {
-            const url = `/api/v1/challenges/${id}`
-            this.params.challenge.cleared = cleared + 1
-            done_at = new Date(done_at); // jsonのdateをjsのDateに変換
-
-            console.log(goal, this.params.challenge.cleared)
-            if (done_at < this.yesterday) { //Doneが押された時の昨日の日付とdone_atを比較
-                continuation = 0 //最後のDoneが昨日より前と判定されたら、continuationを一旦0にする
-            }
-
-            this.params.challenge.continuation = continuation + 1
-            this.params.challenge.done_at = new Date()
-            console.log(this.params.challenge.done_at)
-
-            if (this.params.challenge.cleared >= goal) { // clearedが目標日数をに到達したら終了
-                this.params.challenge.is_done = true
-                console.log("終了")
-            } else {
-                this.params.challenge.is_done = false
-            }
-
-            
-
-            console.log(this.params)
-            this.$axios.put(url, this.params)
-                .then((res) => {
-                    console.log("yeah");
-                    this.getMyChallenges() // challengesを再取得  
-                })
-                .catch((err) => {
-                    console.log("error.")
-                })  
-        },
 
     }
 }
